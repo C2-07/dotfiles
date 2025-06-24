6075909fdf5cec928b457ac9040ddaeb0cd42ab7
c2-07 <114809705+C2-07@users.noreply.github.com>
2025-06-24 16:16:21 +0530

    Fixes : helix theme

--- fish/config.fish
+++ fish/config.fish
@@ -3,19 +3,26 @@ export HOMEBREW_NO_ENV_HINTS=1
 
 # === ALIASES ===
 alias cls='clear'
-alias ll='eza -lah --icons'
-alias la='eza -a --icons'
-alias ls='eza --icons'
-alias g='git'
-alias v='nvim'
-alias zshrc='nvim ~/.zshrc'
-alias reload='source ~/.zshrc'
+alias ls='eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions'
+alias reload='source ~/dotfiles/fish/config.fish'
 alias kc='keychain'
+alias helix='hx'
+alias tree='eza --tree'
+alias rm='rm -i'
+alias cat='bat'
+alias export='set -x VAR'
+alias cd='z'
+alias find='fd'
+alias grep='ripgrep'
 
+zoxide init fish | source
+thefuck --alias | source
 # === KEYBINDINGS ===
 
+# === Theme ===
 set -g fish_color_autosuggestion '#999999'
 
+# For FastFetch Image Setup
 if test "$TERM" = xterm-kitty
     # Terminal is Kitty — supports graphics
     fastfetch --kitty-direct /Users/gourav/.config/fastfetch/shell_logo.png
